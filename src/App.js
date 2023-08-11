ced1668e3b7ba33f14d85da41ba66eab08435655
Ubuntu <ubuntu@ip-172-31-30-103.ec2.internal>
2023-08-11 16:05:54 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import './App.css';
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
+function App() {
+  return(
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      <form>
+      <fieldset>
+         <label>
+           <p>Name</p>
+           <input name="name" />
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
       </form>
-    );
-  }
+    </div>
+  )
 }
+
+export default App;
