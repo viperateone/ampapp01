1ef646279ce15858a7652acb79411c421f92a669
Ubuntu <ubuntu@ip-172-31-30-103.ec2.internal>
2023-08-11 16:18:35 +0000

    t

--- src/App.js
+++ src/App.js
@@ -1,68 +1,38 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-export default class App extends Component {
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-    constructor(props, context) {
-        super(props, context);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.state = { description: '' };
-    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-    onChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
-    onSubmit(e) {
-        e.preventDefault();
-
-        fetch(this.props.formAction, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({description: this.state.description})
-        });
-
-        this.setState({description: ''});
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <form
-                    id="main-login"
-                    action={this.props.action}
-                    method={this.props.method}
-                    onSubmit={this.onSubmit}>
-                    <h2>Admin UI Login</h2>
-                    <label>
-                        <span class="text">user:</span>
-                        <input type="email" name="email"/><br/>
-                    </label>
-                    <br/>
-                    <label>
-                        <span class="text">password:</span>
-                        <input type="password" name="password"/><br/>
-                    </label>
-                    <br/>
-                    <div class="align-right">
-                        <button>Submit</button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-// App.propTypes = { action: React.PropTypes.string.isRequired, method: React.PropTypes.string}
-App.defaultProps = {
-    action: 'http://don.healthedata.com/admin/login',
-    method: 'post'
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<NameForm />);
 
-module.exports = App;
+export default App;
