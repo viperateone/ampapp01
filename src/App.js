162c1a32b122c86de8f7b94c6f34c68bfe823add
Ubuntu <ubuntu@ip-172-31-30-103.ec2.internal>
2023-08-11 16:12:13 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return(
-    <div className="wrapper">
-      <h1>How About Them Apples</h1>
-      <form>
-      <fieldset>
-         <label>
-           <p>Name</p>
-           <input name="name" />
-         </label>
-       </fieldset>
-       <button type="submit">Submit</button>
-      </form>
-    </div>
-  )
+export default class App extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = { description: '' };
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        fetch(this.props.formAction, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({description: this.state.description})
+        });
+
+        this.setState({description: ''});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form
+                    id="main-login"
+                    action={this.props.action}
+                    method={this.props.method}
+                    onSubmit={this.onSubmit}>
+                    <h2>Admin UI Login</h2>
+                    <label>
+                        <span class="text">user:</span>
+                        <input type="email" name="email"/><br/>
+                    </label>
+                    <br/>
+                    <label>
+                        <span class="text">password:</span>
+                        <input type="password" name="password"/><br/>
+                    </label>
+                    <br/>
+                    <div class="align-right">
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
 }
 
-export default App;
+// App.propTypes = { action: React.PropTypes.string.isRequired, method: React.PropTypes.string}
+App.defaultProps = {
+    action: 'http://don.healthedata.com/admin/login',
+    method: 'post'
+};
+
+module.exports = App;
