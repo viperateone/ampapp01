12cb7f9aaa339b7a5a28e819f9598189839659d1
Ubuntu <ubuntu@ip-172-31-30-103.ec2.internal>
2023-08-15 11:47:36 +0000

    1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [mobileNumber, setMobileNumber] = useState("");
   const [message, setMessage] = useState("");
 
+const [name2, setName2] = useState("");
+const [email2, setEmail2] = useState("");
+const [mobileNumber2, setMobileNumber2] = useState("");
+const [message2, setMessage2] = useState("");
+
   let handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -28,6 +33,31 @@ function App() {
         setMessage("User created successfully");
       } else {
         setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  let handleSubmit2 = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://dfez0bzneh5yw.cloudfront.net/acfp", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name2,
+          email: email2,
+          mobileNumber: mobileNumber2,
+        }),
+      });
+     let resJson = await res.json();
+      if (res.status === 200) {
+        setName2("");
+        setEmail2("");
+        setMessage2("User created successfully");
+      } else {
+        setMessage2("Some error occured");
       }
     } catch (err) {
       console.log(err);
@@ -54,6 +84,31 @@ function App() {
           value={mobileNumber}
           placeholder="Mobile Number"
           onChange={(e) => setMobileNumber(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+
+	<form id="login-form" onSubmit={handleSubmit2}>
+        <input
+          type="text"
+          value={name2}
+          placeholder="Name"
+          onChange={(e) => setName2(e.target.value)}
+        />
+        <input
+          type="text"
+          value={email2}
+          placeholder="Email"
+          onChange={(e) => setEmail2(e.target.value)}
+        />
+        <input
+          type="text"
+          value={mobileNumber2}
+          placeholder="Mobile Number"
+          onChange={(e) => setMobileNumber2(e.target.value)}
         />
 
         <button type="submit">Create</button>
