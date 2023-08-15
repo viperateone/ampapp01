de87d45f3600cabb14ccdc42818b461fb2079ba7
Ubuntu <ubuntu@ip-172-31-30-103.ec2.internal>
2023-08-15 11:25:44 +0000

    1

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <head>
 	src="<script type="text/javascript" src="https://83de05343b4f.edge.sdk.awswaf.com/83de05343b4f/8fe518bd50be/challenge.js" defer></script>"
 	</head>
+      const wafIntegration = window.AwsWafIntegration;
       let res = await AwsWafIntegration.fetch("https://dfez0bzneh5yw.cloudfront.net/acfp", {
         method: "POST",
         body: JSON.stringify({
